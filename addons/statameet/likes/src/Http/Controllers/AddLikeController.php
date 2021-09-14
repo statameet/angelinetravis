1487217ee2ba0fe78d038ebb7e46d2aa15f55efe
edalzell <erin@silentz.co>
2021-09-13 19:36:23 -0700

    update likes

--- addons/statameet/likes/src/Http/Controllers/AddLikeController.php
+++ addons/statameet/likes/src/Http/Controllers/AddLikeController.php
@@ -11,6 +11,22 @@ class AddLikeController extends Controller
 {
     public function __invoke(Request $request)
     {
+        /** @var Entry */
+        $entry = EntryFacade::find($request->input('id'));
+
+        $likes = intval($entry->get('likes', 0)) + 1;
+
+        $entry
+            ->set('likes', $likes)
+            ->save();
+
+        if (request()->ajax() || request()->wantsJson()) {
+            return response([
+                'success' => true,
+                'likes' => $likes,
+            ]);
+        }
+
         return back();
     }
 }
